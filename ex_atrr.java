ad203847dd7958858a61cc9d03a2e3b6d7b2d4a6
juliaaa2 <142424951+juliaaa2@users.noreply.github.com>
2023-08-30 11:00:24 -0500

    Add files via upload
    Segundo trabajo en clase 30 de agosto

---
+++ ex_atrr.java
@@ -0,0 +1,9 @@
+package call.unicauca.edu.co;
+
+public class ex_atrr {
+    
+    public String name = "Juliana ";
+    private String document = "10202020";
+    public int year_hbd = 2005;
+
+}
---
+++ exercise_class.java
@@ -0,0 +1,18 @@
+
+package exercise_class;
+
+import call.unicauca.edu.co.ex_atrr;
+
+public class exercise_class {
+
+    public static void main(String[] args) {
+        
+
+        ex_atrr yo = new  ex_atrr();
+        
+        System.out.println("Access var: ");
+        System.out.println("Name import: " + yo.name);
+        System.out.println("Year import: " + yo.year_hbd);
+    }
+    
+}
